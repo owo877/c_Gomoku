df96844a97a88a5ec079123fbb07215aecd016fc
“kiwi” <hikiwi11@gmail.com>
2024-10-05 01:36:23 +0800

    可執行 def有問題

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 void main(int argc, char *argv[]){
     // 初始化
     char chessBoard[19][19];
-    int i, j, n = 0;
-    struct keyValue *anyPiecesL; 
+    int i, j, n = 0, anyPiecesL[19*19][3];
+    // struct keyValue *anyPiecesL; 
     // argv[1] = "Black" or "White"
     // 判斷當前棋色（黒子 1）（白子 0）
     int target = strcmp(argv[1], "Black") == 0 ? 1 : 0; // strcmp ture is 0
@@ -16,24 +16,21 @@ void main(int argc, char *argv[]){
     for(i=0; i<19; i++){
         for(j=0; j<19; j++){
             scanf(" %c", &chessBoard[i][j]);
-            if(strcmp(chessBoard[i][j],".") != 0){
-                anyPiecesL[n].x = i;
-                anyPiecesL[n].y = j;
-                anyPiecesL[n].color = chessBoard[i][j] - '0';
+            if(chessBoard[i][j] != '.'){
+                anyPiecesL[n][0] = i;
+                anyPiecesL[n][1] = j;
+                anyPiecesL[n][2] = chessBoard[i][j] - '0';
                 n++;
             }
         }
     }
-    printf("x: %d y: %d | %d \n", anyPiecesL[0].x, anyPiecesL[0].y, anyPiecesL[0].color);
-    show(chessBoard);
-}
-// 儲存有棋子的list {座標, 顏色（黑 1, 白 0）}
-struct keyValue{
-    int x, y, color;
-};
-// 顯示當前棋盤
-char show(char chessBoard[19][19]){
-    int i, j;
+    // 列印子的位置
+    printf("\n");
+    for(i=0; i<n; i++){
+        printf("x: %d y: %d | %d \n", anyPiecesL[i][0], anyPiecesL[i][1], anyPiecesL[i][2]);
+    }
+    // 列印棋盤
+    printf("\n");
     printf("   A B C D E F G H I J K L M N O P Q R S\n");
     for (i=0; i<19; i++){
         printf("%2d ", i + 1);
@@ -42,7 +39,24 @@ char show(char chessBoard[19][19]){
         }
         printf("\n");
     }
+    // show(chessBoard);
 }
+// 儲存有棋子的list {座標, 顏色（黑 1, 白 0）} error idk
+// struct keyValue{
+//     int x, y, color;
+// };
+// 顯示當前棋盤 error idk
+// void show(char chessBoard[19][19]){
+//     int i, j;
+//     printf("   A B C D E F G H I J K L M N O P Q R S\n");
+//     for (i=0; i<19; i++){
+//         printf("%2d ", i + 1);
+//         for(j=0; j<19; j++){
+//             printf("%c ", chessBoard[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
 // 判斷連線數
 int linkCheck(char chessBoard[19][19], int* target){
     // target[2] = {color(0,1), v(0~7), now[2](x,y)}
